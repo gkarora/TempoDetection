e9f7929186f37b93aac960aba2aca4bfb520cb70
canmanie <canmanie@gmail.com>
2015-10-29 11:20:04 -0400

    Update optflow.cpp

--- Tutorials/optflow.cpp
+++ Tutorials/optflow.cpp
@@ -4,6 +4,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/videoio/videoio.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "OpticalFlow.h"
 
 #include <iostream>
 #include <ctype.h>
