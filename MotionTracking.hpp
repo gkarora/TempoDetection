1d923c288bc2783db2f7e44e8134886e03d23cc4
Gurneet Arora <gurneet.k.arora@gmail.com>
2015-11-28 15:41:18 -0500

    image processing bit

---
+++ MotionTracking.hpp
@@ -0,0 +1,14 @@
+//
+//  MotionTracking.hpp
+//  TempoDetection
+//
+//  Created by Gurneet Arora on 2015-11-26.
+//  Copyright © 2015 Gurneet Arora. All rights reserved.
+//
+
+#ifndef MotionTracking_hpp
+#define MotionTracking_hpp
+
+#include <stdio.h>
+
+#endif /* MotionTracking_hpp */
