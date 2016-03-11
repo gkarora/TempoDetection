845c689d732136769e653305c5104437fa8cd16d
canmanie <ctponnam@uwaterloo.ca>
2016-03-11 12:34:01 -0500

    Adding smooth playback filter

--- ProjFiles/GetBinaryImg.cpp
+++ ProjFiles/GetBinaryImg.cpp
@@ -4,7 +4,6 @@
 
 //
 //
-//  Based on:  objectTrackingTutorial.cpp written by  Kyle Hounslow 2013
 
 //  Copyright © 2015 Gurneet Arora. All rights reserved.
 //
@@ -115,15 +114,21 @@ int get_binary_image(){
             if(recal_counter>=RECALSIZE){
                 int oldTempo = tempo;
                 tempo = getTempo(ypos, fps);
-                if (tempo > 200) {
-                    currentBpm = 200;
-                }
-                else if (tempo<=0){
-                    currentBpm = oldTempo;
-                }
-                else {
+                double factor = 5;
+                if (abs(tempo-oldTempo)< factor)
+                {
                     currentBpm = tempo;
+                } else if (tempo > (oldTempo+factor)) {
+                    currentBpm = oldTempo+factor;
+                    tempo = currentBpm;
+                } else if (tempo < (oldTempo-factor)) {
+                    currentBpm = oldTempo - factor;
+                    tempo = currentBpm;
+                } else {
+                    currentBpm = oldTempo;
+                    tempo = currentBpm;
                 }
+                
                 recal_counter =0;
                 std::cout << currentBpm << ", ";
                 
